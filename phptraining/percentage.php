eb4b87d4e5a6d6c6a09c214df401a210ebb23aed
Deepak <121931668+Deepak-0007@users.noreply.github.com>
2024-02-09 22:28:02 +0530

    Update percentage.php

--- phptraining/percentage.php
+++ phptraining/percentage.php
@@ -5,7 +5,7 @@ $sub1=20;
 $sub2=30;
 $sub3=40;
 $sub4=60;
-$sub5=90;
+$sub5=80;
 $sumOfMarks=$sub1+$sub2+$sub3+$sub4+$sub5;
 $percentage = ($sumOfMarks/500)*100; echo "The percentage are : ". $percentage;
 ?>
